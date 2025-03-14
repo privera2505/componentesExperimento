307a2c9b511a16ab63fe436b8ee47d05389833de
Pablo Rivera <p.riverah@uniandes.edu.co>
2025-03-14 15:49:48 -0500

    Se añadio el envio de correo con un id que llega

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
 from flask import Flask, request, jsonify
 import os
+import smtplib
+
+email_user = os.environ['email_user']
+email_password = os.environ['email_password']
+emails_clients = [
+    {"id": 1, "correo": "p.riverah@uniandes.edu.co"},
+    {"id": 2, "correo": "privera05@hotmail.com"}
+]
+
 
 app = Flask(__name__)
 
 @app.route("/message", methods=["POST"])
 def receive_message():
     data = request.get_json()
-    if not data or "message" not in data:
+    if not data or "id" not in data:
         return jsonify({"error": "Message is required"}), 400
     
-    message = data["message"]
-    print("Received message:", message)
-    return jsonify({"success": True, "received": message})
+    messageID = int(data["id"])
+    print("Received message:", messageID)
+    email_receiver = next((item['correo'] for item in emails_clients if item['id'] == messageID ),None)
+    print(email_receiver)
+    emailSend(email_user,email_password,email_receiver, messageID)
+    return jsonify({"success": True, "received": messageID})
+
+def emailSend(userEmail,password, email_receiver, messageID):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login(userEmail,password)
+
+        smtp.sendmail(userEmail,email_receiver,f"La direccion de entrega de la orden con ID {messageID} ha sido modificada")
