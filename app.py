51439e02062a24c99cc7f7bc3f9e6e933bb59fdf
Pablo Rivera <p.riverah@uniandes.edu.co>
2025-03-14 17:56:01 -0500

    Se añade ruta para bloquear ordendes de compra y otra ruta que las desbloquea

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask, request, jsonify
 import os
 
+orderIdStatus = {
+    1: {"isBlock":False},
+    2:{"isBlock":False}
+}
+
 app = Flask(__name__)
 
 @app.route("/message", methods=["POST"])
 def receive_message():
     data = request.get_json()
-    if not data or "message" not in data:
+    if not data or "id" not in data:
         return jsonify({"error": "Message is required"}), 400
-    
-    message = data["message"]
-    print("Received message:", message)
-    return jsonify({"success": True, "received": message})
+    messageID = int(data["id"])
+    print("Received message:", messageID)
+    orderIdStatus[messageID]["isBlock"] = True
+    print(orderIdStatus)
+    return jsonify({"success": True, "received": messageID})
+
+@app.route('/<variable>')
+def dynamic_route(variable):
+    idNumber = int(variable)
+    orderIdStatus[idNumber]["isBlock"] = False
+    print(orderIdStatus)
+    return f"Has desbloqueado la orden de entrega con ID: {variable}."
+
+@app.route('/favicon.ico')
+def favicon():
+    return '', 204  # Responde con un código 204 (sin contenido)
